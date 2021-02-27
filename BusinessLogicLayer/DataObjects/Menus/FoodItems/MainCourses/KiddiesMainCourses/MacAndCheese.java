a07aac9723a3e472311499a21b4cf61eda7f030c
Thato-Raphahlelo <thatomraphahlelo@gmail.com>
2021-02-27 17:56:21 +0200

    MacAndCheese details

--- BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/KiddiesMainCourses/MacAndCheese.java
+++ BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/KiddiesMainCourses/MacAndCheese.java
@@ -1,5 +1,19 @@
 package BusinessLogicLayer.DataObjects.Menus.FoodItems.MainCourses.KiddiesMainCourses;
 
-public class MacAndCheese {
-    
+import BusinessLogicLayer.Abstractions.KiddiesMainCourse;
+
+public class MacAndCheese extends KiddiesMainCourse {
+
+    @Override
+    public String getItemName() {
+
+        return "Mac and Cheese";
+    }
+
+    @Override
+    public double getPrice() {
+
+        return 45.99;
+    }
+
 }

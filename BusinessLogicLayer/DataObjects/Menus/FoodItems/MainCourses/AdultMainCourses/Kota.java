49935360279ef7a3a800713cc57b6336e4d97474
Thato-Raphahlelo <thatomraphahlelo@gmail.com>
2021-02-27 17:44:48 +0200

    Kota details

--- BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/AdultMainCourses/Kota.java
+++ BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/AdultMainCourses/Kota.java
@@ -1,5 +1,19 @@
 package BusinessLogicLayer.DataObjects.Menus.FoodItems.MainCourses.AdultMainCourses;
 
-public class Kota {
-    
+import BusinessLogicLayer.Abstractions.AdultMainCourse;
+
+public class Kota extends AdultMainCourse {
+
+    @Override
+    public String getItemName() {
+
+        return "Sausage, Cheese, fries in a half loaf";
+    }
+
+    @Override
+    public double getPrice() {
+
+        return 15.99;
+    }
+
 }

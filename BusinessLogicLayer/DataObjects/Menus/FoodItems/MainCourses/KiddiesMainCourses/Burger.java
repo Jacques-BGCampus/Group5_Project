681bdd73b86e8cdb188232e01f0699c6b199f511
Thato-Raphahlelo <thatomraphahlelo@gmail.com>
2021-02-27 17:53:49 +0200

    Burger details

--- BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/KiddiesMainCourses/Burger.java
+++ BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/KiddiesMainCourses/Burger.java
@@ -1,5 +1,19 @@
 package BusinessLogicLayer.DataObjects.Menus.FoodItems.MainCourses.KiddiesMainCourses;
 
-public class Burger {
-    
+import BusinessLogicLayer.Abstractions.KiddiesMainCourse;
+
+public class Burger extends KiddiesMainCourse {
+
+    @Override
+    public String getItemName() {
+
+        return "Beef Burger with emmenthal cheese and Adobo sauce";
+    }
+
+    @Override
+    public double getPrice() {
+
+        return 89.99;
+    }
+
 }

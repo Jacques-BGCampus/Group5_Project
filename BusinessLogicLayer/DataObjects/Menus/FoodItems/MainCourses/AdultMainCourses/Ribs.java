7f0b394104e2bdf6863d9392a94c63030e565067
Thato-Raphahlelo <thatomraphahlelo@gmail.com>
2021-02-27 17:49:08 +0200

    Ribs details

--- BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/AdultMainCourses/Ribs.java
+++ BusinessLogicLayer/DataObjects/Menus/FoodItems/MainCourses/AdultMainCourses/Ribs.java
@@ -1,5 +1,19 @@
 package BusinessLogicLayer.DataObjects.Menus.FoodItems.MainCourses.AdultMainCourses;
 
-public class Ribs {
-    
+import BusinessLogicLayer.Abstractions.AdultMainCourse;
+
+public class Ribs extends AdultMainCourse {
+
+    @Override
+    public String getItemName() {
+
+        return "Short Ribs basted in a BBQ sauce";
+    }
+
+    @Override
+    public double getPrice() {
+
+        return 199.99;
+    }
+
 }
